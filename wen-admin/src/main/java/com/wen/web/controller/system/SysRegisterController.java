48b8f53c1fcfaa7228533f01efba221f38e70330
wenjie <cbincr427@163.com>
2025-12-24 03:58:55 +0800

    新ruoyi+删除log

---
+++ wen-admin/src/main/java/com/wen/web/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.wen.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.wen.common.core.controller.BaseController;
+import com.wen.common.core.domain.AjaxResult;
+import com.wen.common.core.domain.model.RegisterBody;
+import com.wen.common.utils.StringUtils;
+import com.wen.framework.web.service.SysRegisterService;
+import com.wen.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
